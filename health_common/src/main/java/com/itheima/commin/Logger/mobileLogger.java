f89103073761cc60aaff67b97499ec80815c75ba
merlin <22692762@qq.com>
2019-08-02 18:29:05 +0800

    version1.0 骨架版本

---
+++ health_common/src/main/java/com/itheima/commin/Logger/mobileLogger.java
@@ -0,0 +1,10 @@
+package com.itheima.commin.Logger;
+
+/**
+ * @Author: Merlin cheung
+ * @Date: Creat in 10:55 2019/7/28
+ */
+public class mobileLogger {
+
+
+}
---
+++ health_common/src/main/java/com/itheima/commin/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.itheima.commin;
+
+/**
+ * @Author: Merlin cheung
+ * @Date: Creat in 8:33 2019/7/22
+ */
+public class RedisConstant {
+    public static final String SETMEAL_PIC_RESOURCES = "packagePicResources";
+    //套餐图片保存在数据库中的图片名称
+    public static final String SETMEAL_PIC_DB_RESOURCES = "packagePicDbResources";
+}
